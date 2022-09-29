21dceb7809d5ac21effb02cd665f5dc8f237fb80
Matsura Yuma <lemonburst1958@gmail.com>
2022-09-29 18:03:48 +0900

    Finish TweetComposer

--- src/components/TweetComposer.tsx
+++ src/components/TweetComposer.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+import { SvgComponent } from "../types";
 import Avatar from "./Avatar";
 import styles from "./TweetComposer.module.scss";
+import { ReactComponent as ImageOutlineSvg } from "../icons/image-outline.svg";
+import { ReactComponent as GifOutlineSvg } from "../icons/gif-outline.svg";
+import { ReactComponent as PollOutlineSvg } from "../icons/poll-outline.svg";
+import { ReactComponent as EmojiOutlineSvg } from "../icons/emoji-outline.svg";
+import { ReactComponent as ScheduleOutlineSvg } from "../icons/schedule-outline.svg";
+import { ReactComponent as LocationOutlineSvg } from "../icons/location-outline.svg";
+import Button from "./Button";
 
 type Props = unknown;
 
@@ -9,8 +16,65 @@ export default function TweetComposer(props: Props) {
     <div className={styles.container}>
       <Avatar size={48} />
       <div>
-        <textarea placeholder="What's happening?" className={styles.textarea} />
+        <div className={styles.upper}>
+          <textarea
+            placeholder="What's happening?"
+            className={styles.textarea}
+          />
+        </div>
+        <div className={styles.lower}>
+          <Toolbar />
+          <div className={styles.tweetWrap}>
+            <Button>Tweet</Button>
+          </div>
+        </div>
       </div>
     </div>
+  );
+}
+
+type ToolbarItem = {
+  SvgIcon: SvgComponent;
+  title: string;
+};
+
+const toolbarItems: readonly ToolbarItem[] = [
+  {
+    SvgIcon: ImageOutlineSvg,
+    title: "Media",
+  },
+  {
+    SvgIcon: GifOutlineSvg,
+    title: "GIF",
+  },
+  {
+    SvgIcon: PollOutlineSvg,
+    title: "Poll",
+  },
+  {
+    SvgIcon: EmojiOutlineSvg,
+    title: "Emoji",
+  },
+  {
+    SvgIcon: ScheduleOutlineSvg,
+    title: "Schedule",
+  },
+  {
+    SvgIcon: LocationOutlineSvg,
+    title: "Location",
+  },
+];
+
+function Toolbar() {
+  return (
+    <ul className={styles.toolbar}>
+      {toolbarItems.map((item) => (
+        <li>
+          <button className={styles.toolbar__button}>
+            <item.SvgIcon className={styles.toolbar__icon} />
+          </button>
+        </li>
+      ))}
+    </ul>
   );
 }
