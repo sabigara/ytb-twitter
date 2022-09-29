2e591256e068e085888eca5def70171ef9c87fcb
Matsura Yuma <lemonburst1958@gmail.com>
2022-09-29 15:37:39 +0900

    Add Logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
-import styles from "./Header.module.scss";
+import { ReactComponent as TwitterFillSvg } from "../icons/twitter-fill.svg";
 import Navigation from "./Navigation";
+import styles from "./Header.module.scss";
 
 export default function Header() {
   return (
     <header className={styles.container}>
+      <h1 className={styles.logo}>
+        <a href="/home">
+          <TwitterFillSvg />
+        </a>
+      </h1>
       <Navigation />
     </header>
   );
